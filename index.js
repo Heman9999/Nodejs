662b41729fa8ede5e8105e20888e937dd9b0cee1
Heman9999 <32823059+Heman9999@users.noreply.github.com>
2023-07-19 10:52:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    if(req === '/'){
-        // res.writeHead(200, {"Context-Type":""})
-        res.end("<h1>Hello World</h1>")
-    }else if(req === "/about"){
-        res.end(`<h1>About</h1>`)
-    }
+    
+    res.end("<h1>Hello World</h1>")
 })
 
-const PORT = 8080;
+const PORT = 3000;
 
 server.listen(PORT,()=> console.log(`Started Server`))
